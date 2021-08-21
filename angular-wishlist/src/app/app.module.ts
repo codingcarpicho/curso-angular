51833c72159310610ccf8c6410f528d165697be6
codingcarpicho <nekosenshi1@gmail.com>
2021-08-21 15:30:05 -0300

    'Organizar rutas de forma modular'

--- angular-wishlist/src/app/app.module.ts
+++ angular-wishlist/src/app/app.module.ts
@@ -21,18 +21,35 @@ import { LoginComponent } from './components/login/login/login.component';
 import { ProtectedComponent } from './components/protected/protected/protected.component';
 import { UsuarioLogueadoGuard } from './guards/usuario-logueado/usuario-logueado.guard';
 import { AuthService } from './services/auth.service';
+import { VuelosComponentComponent } from './components/vuelos/vuelos-component/vuelos-component.component';
+import { VuelosMainComponentComponent } from './components/vuelos/vuelos-main-component/vuelos-main-component.component';
+import { VuelosMasInfoComponentComponent } from './components/vuelos/vuelos-mas-info-component/vuelos-mas-info-component.component';
+import { VuelosDetalleComponentComponent } from './components/vuelos/vuelos-detalle-component/vuelos-detalle-component.component';
 
 // definiendo direcciones del nav
+
+export const childrenRoutesVuelos: Routes = [
+  { path: '', redirectTo: 'main', pathMatch: 'full'},
+  { path: 'main', component: VuelosMainComponentComponent},
+  { path: 'mas-info', component: VuelosMasInfoComponentComponent},
+  { path: ':id', component: VuelosDetalleComponentComponent},
+];
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full'},
   { path: 'home', component: ListaDestinoComponent},
   { path: 'destino/:id', component: DestinoDetalleComponent},
-  {path: 'login', component: LoginComponent} ,
+  { path: 'login', component: LoginComponent} ,
   {
     path: 'protected',
     component: ProtectedComponent,
     canActivate: [UsuarioLogueadoGuard]
+  },
+  {
+    path: 'vuelos',
+    component: VuelosComponentComponent,
+    canActivate: [ UsuarioLogueadoGuard ],
+    children: childrenRoutesVuelos
   }
 ];
 
@@ -59,6 +76,10 @@ let reducersInitialState = {
     FormDestinoViajeComponent,
     LoginComponent,
     ProtectedComponent,
+    VuelosComponentComponent,
+    VuelosMainComponentComponent,
+    VuelosMasInfoComponentComponent,
+    VuelosDetalleComponentComponent,
     
   ],
   imports: [
